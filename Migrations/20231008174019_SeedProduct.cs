f9105551f376c7d90d35d9a6abcd33733bbba23b
Yan Souza <yansouza009@gmail.com>
2023-10-08 15:03:04 -0300

    feat: seed database

---
+++ Migrations/20231008174019_SeedProduct.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedProduct : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder mb)
+        {
+            mb.Sql("Insert into Categories(Name, ImageUrl) Values('Bebidas', 'Bebida.jpg')");
+            mb.Sql("Insert into Categories(Name, ImageUrl) Values('Higiene', 'Higiene.jpg')");
+            mb.Sql("Insert into Categories(Name, ImageUrl) Values('Sobremesas', 'Sobremesas.jpg')");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder mb)
+        {
+            mb.Sql("Delete from Categories");
+        }
+    }
+}
---
+++ Migrations/20231008174804_SeedCategorie.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedCategorie : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder mb)
+        {
+            mb.Sql("Insert into Procucts(Name, Description, Price, ImageUrl, Stock, DateRegister, CategoryId) " +
+            "Values('Coca-Cola', 'Refrigerante de Cola 1000ML', '6.00', 'cocacola.jpg', 30, GetDate(),1)");
+             mb.Sql("Insert into Procucts(Name, Description, Price, ImageUrl, Stock, DateRegister, CategoryId) " + 
+             "Values('Papel Higiênico', 'Papel Higiênico Neve Folha Dupla', '17.50', 'papel.jpg', 30, GetDate(),2)" );
+             mb.Sql("Insert into Procucts(Name, Description, Price, ImageUrl, Stock, DateRegister, CategoryId) " + 
+             "Values('KitKat', 'Chocolate Kit-Kat', '4.00', 'kitkat.jpg', 30, GetDate(),3)" );
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder mb)
+        {
+            mb.Sql("Delete from Products");
+        }
+    }
+}

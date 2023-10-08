874fd7784644877aa190571f3592d0050a83c371
Yan Souza <yansouza009@gmail.com>
2023-10-08 15:25:53 -0300

    feat: adicionando método get

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -1,14 +1,27 @@
 using Microsoft.AspNetCore.Mvc;
 using WebApi.Context;
+using WebApi.models;
 
 namespace WebApi.Controllers;
 
 [ApiController]
 [Route("[controller]")]
-public class ProductController : ControllerBase{
+public class ProductController : ControllerBase
+{
     private readonly AppDbContext _context;
 
-    public ProductController(AppDbContext context){
+    public ProductController(AppDbContext context)
+    {
         _context = context;
+    }
+    [HttpGet]
+    public ActionResult<IEnumerable<Product>> Get()
+    {
+        var product = _context.Procucts.ToList();
+        if (product is null)
+        {
+            return NotFound("Produtos não encontrados");
+        }
+        return product;
     }
 }
